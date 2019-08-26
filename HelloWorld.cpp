6414bcabaaaea245761f6aa3943486f5a349b33e
William Botura <williamdigitalwork@gmail.com>
2019-08-26 18:18:08 -0300

    first commit and course start

---
+++ HelloWorld.cpp
@@ -0,0 +1,19 @@
+//directive pre-processor for all code library in cpp
+#include <iostream>
+
+//starts with a int Main
+//int main()
+//{
+//	//variables and Data Types:
+//
+//	//store integer numbers
+//	int i = -3;
+//	//store only positive integer numbers
+//	unsigned int ui = 10;
+//	//double is used instead of the float at software development, because it can store more decimal numbers than the float
+//	double d = 2.7;
+//	bool flag = true;
+//
+//	std::cout << "Hello World!";
+//	return 0;
+//}
